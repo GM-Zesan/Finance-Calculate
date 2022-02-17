18c59da14ca95c43828a854880ba6c24af54c460
Zesan <gmzesan7767@gmail.com>
2022-02-17 12:46:35 +0600

    full complete with validation and error message

--- js/app.js
+++ js/app.js
@@ -1,41 +1,65 @@
+//this is function 
 function myfinance(isExpense){
     const getIncome = document.getElementById('my-income');
     const myIncome = parseFloat(getIncome.value);
     const foodCost = parseFloat(document.getElementById('ex-food').value);
     const rentCost = parseFloat(document.getElementById('ex-rent').value);
     const clothCost = parseFloat(document.getElementById('ex-cloth').value);
-    const errorMsg = document.getElementById('error-msg');
+    const errorMsgIncome = document.getElementById('error-msg-income');
+    const errorMsgNull = document.getElementById('error-msg-null');
+    const errorMsgNegative = document.getElementById('error-msg-expense');
+    const errorMsgSave = document.getElementById('error-msg-save');
     const totalCost = foodCost+rentCost+clothCost;
     const myBalance = myIncome-totalCost;
-    if(myIncome > 0){
-        errorMsg.style.display = "none";
-        if(isExpense == true){
-            if(myBalance>=0){
-                document.getElementById('total-cost').innerText = totalCost;
-                document.getElementById('balance').innerText = myBalance;
+    if(foodCost>=0 && rentCost>=0 && clothCost>=0){
+        errorMsgNull.style.display = "none";
+        errorMsgNegative.style.display = "none";
+        if(myIncome > 0){
+            errorMsgIncome.style.display = "none";
+            if(isExpense == true){
+                if(myBalance>=0){
+                    document.getElementById('total-cost').innerText = totalCost;
+                    document.getElementById('balance').innerText = myBalance;
+                }
+                else{
+                    document.getElementById('total-cost').innerText = "Income is not sufficient";
+                    document.getElementById('balance').innerText = myIncome;
+                }
             }
             else{
-                document.getElementById('total-cost').innerText = "Income is not sufficient";
-                document.getElementById('balance').innerText = myIncome;
+                const saveParcent = parseFloat(document.getElementById('save-percent').value);
+                if(saveParcent>=0){
+                    errorMsgSave.style.display = "none";
+                    const mySavings = myIncome*(saveParcent/100);
+                    if(myBalance>=mySavings){
+                        document.getElementById('remain').innerText = myBalance-mySavings;
+                        document.getElementById('my-savings').innerText = mySavings;
+                    }
+                    else{
+                        document.getElementById('remain').innerText = myBalance;
+                        document.getElementById('my-savings').innerText = "do not have sufficient Taka";
+                    }
+                }else{
+                    errorMsgSave.style.display = "block";
+                    document.getElementById('remain').innerText = myBalance;
+                    document.getElementById('my-savings').innerText = "❌❌❌";
+                }
             }
+        }else{
+
+            errorMsgIncome.style.display = "block";
+            getIncome.value = '';
         }
-        else{
-            const saveParcent = parseFloat(document.getElementById('save-percent').value);
-            const mySavings = myIncome*(saveParcent/100);
-            if(myBalance>=mySavings){
-                document.getElementById('remain').innerText = myBalance-mySavings;
-                document.getElementById('my-savings').innerText = mySavings;
-            }
-            else{
-                document.getElementById('remain').innerText = myBalance;
-                document.getElementById('my-savings').innerText = "do not have sufficient Taka";
-            }
-            
-        }
-    }else{
-        errorMsg.style.display = "block";
-        getIncome.value = '';
+
     }
+    else if(foodCost<0 || rentCost<0 || clothCost<0){
+        errorMsgNull.style.display = "none";
+        errorMsgNegative.style.display = "block";
+    }
+    else if(foodCost == null || foodCost == "" || rentCost == null || rentCost == ""|| clothCost == null || clothCost == ""){
+        errorMsgNull.style.display = "block";
+        errorMsgNegative.style.display = "none";
+    } 
 }
 
 document.getElementById('calclute').addEventListener('click',function(){
